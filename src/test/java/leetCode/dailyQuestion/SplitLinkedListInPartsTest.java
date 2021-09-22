31a36c9ea838aae25faa0f085226b00d4193fb54
JokerBingbin <70361772+JokerBingbin@users.noreply.github.com>
2021-09-22 14:38:05 +0800

    # 9.22 每日一题 分隔链表

--- src/test/java/leetCode/dailyQuestion/SplitLinkedListInPartsTest.java
+++ src/test/java/leetCode/dailyQuestion/SplitLinkedListInPartsTest.java
@@ -10,8 +10,6 @@ public class SplitLinkedListInPartsTest {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
         SplitLinkedListInParts solution = new SplitLinkedListInParts();
         ListNode[] list = solution.splitListToParts(head, 5);
-        for (ListNode node : list) {
-            System.out.println(node.val);
-        }
+
     }
 }
