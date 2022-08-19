b25a1fc2954089e90eff117dca9e8aac50890ebe
lukarzhang <lukarzhang@tencent.com>
2022-08-19 08:48:24 +0800

    2022.8.19 每日一题 在既定时间写作业的学生数量

---
+++ src/main/java/leetCode/dailyQuestion/numberOfStudentsDoingHomeworkAtAGivenTime/Solution.java
@@ -0,0 +1,15 @@
+package leetCode.dailyQuestion.numberOfStudentsDoingHomeworkAtAGivenTime;
+
+public class Solution {
+
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int res = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                res ++;
+            }
+        }
+        return res;
+    }
+}
